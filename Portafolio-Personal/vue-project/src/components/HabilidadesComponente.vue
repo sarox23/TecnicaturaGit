e74748542c9556ce6a802d9f0cb0e96883d5f20e
sarox23 <holiwisas125@gmail.com>
2024-11-03 12:49:47 -0300

    subo clase 8 Experiencia  y clase 9 Habilidades

---
+++ Portafolio-Personal/vue-project/src/components/HabilidadesComponente.vue
@@ -0,0 +1,109 @@
+<script setup>
+import img1 from '/src/components/icons/js.svg';
+import img2 from '/src/components/icons/python.svg';
+import img3 from '/src/components/icons/html-css.svg';
+import img4 from '/src/components/icons/java.svg';
+
+import { ref } from 'vue';
+
+const habilidades = ref([
+    {
+        id: 1, nombre: 'Lenguajes de Programación', habilidades: [
+            { id: 1, nombre: 'JavaScript', nivel: 'Basico', icono: img1 },
+            { id: 2, nombre: 'Python', nivel: 'Intermedio', icono: img2 },
+            { id: 3, nombre: 'HTML/CSS', nivel: 'Basico', icono: img3 },
+            { id: 4, nombre: 'Java', nivel: 'Basico', icono: img4 },
+        ]
+    },
+
+]);
+
+</script>
+
+
+<template>
+    <div class="skills-contenedor">
+        <div v-for="habilidad in habilidades" :key="habilidad.id" class="skills-categoria">
+            <h3>{{ habilidad.nombre }}</h3>
+            <ul class="skills">
+                <li v-for="habilidad in habilidad.habilidades" :key="habilidad.id" class="skill">
+                    <img :src="habilidad.icono" :alt="habilidad.nombre">
+                    <span>{{ habilidad.nombre }}: {{ habilidad.nivel }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+.skills-contenedor {
+    padding: 2rem;
+    background: linear-gradient(-45deg, #021526, #03346E, #6EACDA, #E2E2B6);
+
+}
+
+.skills-categoria {
+    margin-bottom: 20px;
+}
+
+.skills-categoria h3 {
+    display: flex;
+    justify-content: left;
+    margin-bottom: 10px;
+    font-size: 1.5em;
+    color: aliceblue;
+    font-size: 1.5em;
+    font-weight: bold;
+}
+
+.skills {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 20px;
+}
+
+.skill {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background-color: rgba(241, 245, 243, 0.856);
+    padding: 0.3em;
+    border-radius: 8px;
+    box-shadow: 0px 4px 12px rgba(2, 151, 151, 0.963);
+    flex: 1 1 200px;
+    max-width: 250px;
+}
+
+.skill:hover {
+    background-color: rgba(2, 151, 151, 0.963);
+}
+
+.skill img {
+    width: 35px;
+    height: 35px;
+}
+
+.skill span {
+    font-size: 1em;
+    color: #333;
+}
+
+.skill:hover {
+    font-size: 1.3em;
+    transition: 2s ease;
+}
+
+@media (max-width: 768px) {
+    .skills-categoria h3 {
+        justify-content: center;
+    }
+
+    .skill {
+        justify-content: center;
+    }
+}
+</style>
---
+++ Portafolio-Personal/vue-project/src/components/ProyectosComponente.vue
@@ -0,0 +1,145 @@
+<script setup>
+import proyecto1 from '/src/assets/restaurant.png';
+import proyecto2 from '/src/assets/e-learning.png';
+import proyecto3 from '/src/assets/blog.png';
+import proyecto4 from '/src/assets/restaurant.png';
+
+// Este componente muestra una galería de proyectos con información sobre cada proyecto
+import { ref } from 'vue';
+//.ref es una función que se utiliza para crear una referencia reactiva en Vue 3
+var misProyectos = ref([]);
+//.value es una propiedad que se utiliza para acceder al valor de una referencia reactiva en Vue 3
+misProyectos.value = ([
+    //esto es un array de objetos que contiene información sobre los proyectos
+    {
+        id: 1,
+        src: proyecto1,
+        titulo: "Base de datos en Pseint (2024)",
+        descripcion: "Creamos un proyecto de Pseint con mi grupo de facultad",
+        projectoLink: "https://www.google.com.ar",
+        githubLink: "https://github.com/usuario/proyecto"
+    },
+])
+
+</script>
+
+<template>
+    <!-- Aquí va el código HTML de la galería de proyectos -->
+    <div class="galeria">
+        <!-- Itera sobre cada proyecto en la lista de misProyectos -->
+        <!--key es una propiedad especial que se utiliza para identificar de forma única cada elemento en una lista de Vue.js-->
+        <li class="proyecto" v-for="proyecto in misProyectos" :key="proyecto.id">
+            <!-- Muestra la imagen del proyecto utilizando : que es una directiva de enlace de atributo en Vue.js -->
+            <img :src="proyecto.src" :alt="proyecto.titlulo">
+            <div class="proyecto-info">
+                <!-- Muestra el título y la descripción del proyecto utilizando la interpolación de Vue.js -->
+                <h3>{{ proyecto.titulo }}</h3>
+                <p>{{ proyecto.descripcion }}</p>
+                <div class="proyecto-links">
+                    <a :href="proyecto.projectoLink" class="btn-ver-mas" target="_blank" rel="noopener noreferrer">Ver
+                        Proyecto</a>
+                    <a :href="proyecto.githubLink" class="github-link" target="_blank" rel="noopener noreferrer">Ver
+                        Código en GitHub</a>
+                </div>
+            </div>
+        </li>
+    </div>
+</template>
+
+<style scoped>
+
+.galeria {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    padding: 20px;
+    justify-content: center;
+    background: linear-gradient(-45deg, #021526, #03346E, #6EACDA, #E2E2B6);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+
+    0% {
+        background-position: 0% 50%;
+    }
+
+    50% {
+        background-position: 100% 50%;
+    }
+
+    100% {
+        background-position: 0% 50%;
+    }
+}
+
+.proyecto {
+    display: flex;
+    flex-direction: column;
+    border: 2px solid #ddd;
+    border-radius: 8px;
+    overflow: hidden;
+    background-color: #f9f9f9;
+    max-width: 222px;
+    flex: 1 1 300px;
+}
+
+.proyecto img {
+    width: 100%;
+    height: auto;
+    display: block;
+}
+
+.proyecto-info {
+    padding: 15px;
+    text-align: center;
+}
+
+
+.proyecto-info h3 {
+    margin: 10px 0;
+    font-size: 1.3em;
+    color: #333;
+}
+
+.proyecto-info p {
+    margin: 10px 0;
+    font-size: 1em;
+    color: #666;
+}
+
+.proyecto-links {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 30px;
+
+}
+
+.proyecto-links .btn-ver-mas {
+    background-color: #007BFF;
+    color: #fff;
+    padding: 10px 15px;
+    border-radius: 5px;
+    text-decoration: none;
+    transition: background-color 0.3s;
+    margin-top: 1rem;
+}
+
+.proyecto-links .btn-ver-mas:hover {
+    background-color: #0056b3;
+}
+
+.proyecto-links .github-link {
+    color: #333;
+    text-decoration: none;
+    font-size: 0.9em;
+}
+
+.proyecto-links .github-link:hover {
+    text-decoration: underline;
+}
+</style>
